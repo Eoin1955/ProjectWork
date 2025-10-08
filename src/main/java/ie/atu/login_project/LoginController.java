3499bacef36c09e7a94e011caf82b38c5fd81cf0
EOINAGER-STUDENT <g00431577@atu.ie>
2025-10-08 14:07:29 +0100

    login class private username and password

--- src/main/java/ie/atu/login_project/LoginController.java
+++ src/main/java/ie/atu/login_project/LoginController.java
@@ -1,5 +1,11 @@
 package ie.atu.login_project;
 
-public class LoginController {
+import lombok.AllArgsConstructor;
+import lombok.Data;
 
+@Data
+@AllArgsConstructor
+public class LoginController {
+    private String username;
+    private String password;
 }
