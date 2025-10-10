7eb408412c7ce9753a638021efb4283560967dd5
eoin ager <g00431577@atu.ie>
2025-10-10 23:54:51 +0100

    validated user inputs and now only registered users can enter into more details

--- src/main/java/ie/atu/login_project/Login.java
+++ src/main/java/ie/atu/login_project/Login.java
@@ -1,11 +1,15 @@
 package ie.atu.login_project;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 
 @Data
 @AllArgsConstructor
 public class Login {
+    @Size(min = 1, max = 20, message = "larger than 20 characters or blank")
     private String username;
+    @Size(min = 1, max = 20, message = "larger than 20 characters or blank")
     private String password;
 }
