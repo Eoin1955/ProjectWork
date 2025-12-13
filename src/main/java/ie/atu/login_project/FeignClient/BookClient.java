2d5a42759c56d223387d4695d2ce14048e8be9b4
eoin ager <g00431577@atu.ie>
2025-12-13 23:38:34 +0000

    Openfeign

---
+++ src/main/java/ie/atu/login_project/FeignClient/BookClient.java
@@ -0,0 +1,17 @@
+package ie.atu.login_project.FeignClient;
+
+
+import ie.atu.login_project.DTO.BookDTO;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+@FeignClient(
+        name = "book-service",
+        url = "${book.service.base-url}"
+)
+
+public interface BookClient {
+    @GetMapping("/api/book/{id}")
+    BookDTO getBookID(@PathVariable Long id);
+}
